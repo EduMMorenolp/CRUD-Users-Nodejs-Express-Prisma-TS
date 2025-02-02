aa730ec62f10d130fa079f74de4474667c955919
Edu Moreno <e.m.morenolp@gmail.com>
2025-02-02 20:32:33 -0300

    check de data base online

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,31 @@
-// ./src/server.js
+// ./src/server.ts
 
-import app from './app.js';
-import dotenv from 'dotenv';
+import app from "./app.js";
+import dotenv from "dotenv";
+import { checkDatabaseConnection } from "./config/checkDatabase.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || 'localhost';
+const HOST = process.env.HOST || "localhost";
 
-// Conectar a la base de datos
+// Función para arrancar el servidor
+async function startServer() {
+  const isDatabaseOnline = await checkDatabaseConnection();
 
-// Arrancar el servidor
-app.listen(PORT, () => {
-  console.log('\n==================================================');
-  console.log(`🚀 Servidor corriendo en: http://${HOST}:${PORT}`);
-  console.log(`📃 Swagger Docs: http://${HOST}:${PORT}/api-docs`);
-  console.log('==================================================\n');
-});
+  if (!isDatabaseOnline) {
+    console.error("🚨 El servidor no puede iniciar. Base de datos offline.");
+    process.exit(1);
+  }
 
+  // Arrancar el servidor
+  app.listen(PORT, () => {
+    console.log("\n==================================================");
+    console.log(`🚀 Servidor corriendo en: http://${HOST}:${PORT}`);
+    console.log(`📃 Swagger Docs: http://${HOST}:${PORT}/api-docs`);
+    console.log("==================================================\n");
+  });
+}
 
+// Iniciar el servidor
+startServer();
